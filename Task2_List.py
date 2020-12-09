2ab45ab9771b4f727cf147e7a73febb216d756b1
Ker4ik8 <74976084+Ker4ik8@users.noreply.github.com>
2020-12-09 13:55:11 +0500

    09.12 Task2

---
+++ Task2_List.py
@@ -0,0 +1,12 @@
+print("Введите количество чисел, которое хотите ввести.")
+n = int(input())
+print("Введите числа в одну строку.")
+a = [int(i) for i in input().split()]
+a_list = list(a)
+b_list = list()
+b = 0
+for i in range(0, (n // 2) * 2, 2):
+    b = a_list[i]
+    a_list[i] = a_list[i + 1]
+    a_list[i + 1] = b
+print(a_list)
