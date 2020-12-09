0da54c9946c66c9459b2ba76f351411c7992e115
Ker4ik8 <74976084+Ker4ik8@users.noreply.github.com>
2020-12-09 12:11:26 +0300

    09.12 Task3

--- Task3_Set.py
+++ Task3_Set.py
@@ -1,8 +1,8 @@
 print("Введите количество чисел, которое хотите ввести.")
 n = int(input())
-print("Введите числа в строку.")
+print("Введите числа в одну строку.")
 a = [int(i) for i in input().split()]
-a_list = list(a)
-res = [int(item) for item in a_list]
+a_set = set(a)
+res = [int(item) for item in a_set]
 newSet = set(res)
 print(len(newSet))
